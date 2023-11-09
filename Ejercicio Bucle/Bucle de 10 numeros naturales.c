414a6da6550c545c8770c9c1d5a61c6c058f7814
Williamsordo <150317194+Williamsordo@users.noreply.github.com>
2023-11-09 01:08:07 -0400

    Add files via upload

---
+++ Ejercicio Bucle/Bucle de 10 numeros naturales.c
@@ -0,0 +1,16 @@
+//*Imprima los primeros 10 números naturales usando el bucle while*//
+
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
+
+
